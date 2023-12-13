366ebd038084347f89c6795dfd0300ff64602d98
Abhishek-Chougule <chouguleabhis@gmail.com>
2023-12-13 14:11:17 +0000

    Fix

--- whitelabel/whitelabel/doctype/whitelabel_setting/test_whitelabel_setting.py
+++ whitelabel/whitelabel/doctype/whitelabel_setting/test_whitelabel_setting.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2021, Bhavesh Maheshwari and Contributors
+# Copyright (c) 2021, Abhishek Chougule and Contributors
 # See license.txt
 from __future__ import unicode_literals
 
