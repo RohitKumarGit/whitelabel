013d10fd5eec56de24ab9d183c52f75fbfaa9b20
Rishabh Pandram <rishabh@onehash.ai>
2025-01-08 18:46:32 +0530

    updated system settings

--- whitelabel/api.py
+++ whitelabel/api.py
@@ -15,6 +15,7 @@ def whitelabel_patch():
 	brand_name = frappe.get_hooks("brand_name")[0]
 	update_onboard_details(brand_name)
 	update_website_settings(brand_name)
+	update_system_settings(brand_name)
 	
 def boot_session(bootinfo):
 	"""boot session - send website info if guest"""
@@ -31,7 +32,12 @@ def update_field_label():
 	frappe.db.sql("""Update `tabDocField` set label='OneHash' where fieldname='erpnext_user' and parent='Employee'""")
 
 def update_website_settings(brand_name):
-	frappe.db.set_value("Website Settings", "Website Settings", "app_name", brand_name)
+	website_settings = frappe.get_doc("Website Settings", "Website Settings")
+	website_settings.db_set("app_name", brand_name, commit=True)
+
+def update_system_settings(brand_name):
+	frappe.db.set_value("System Settings", "System Settings", "otp_issuer_name", brand_name)
+	frappe.db.commit()
 
 def update_onboard_details(brand_name):
 	update_onboard_module(brand_name)
